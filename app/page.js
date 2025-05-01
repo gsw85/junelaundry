1bdcf26f2674cf01d7ae1ac9839f65e168d0ee59
SW. G. <gsw.thc@gmail.com>
2025-05-01 12:47:20 +0800

    update

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import Hero from "@/app/(comp)/hero";
+import Footer from "@/components/footer";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <Footer />
     </main>
   );
 }
