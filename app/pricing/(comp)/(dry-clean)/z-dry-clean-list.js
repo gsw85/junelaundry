52952dad72f54f54c5346fdf8cc4146e6c0e3d54
SW. G. <gsw.thc@gmail.com>
2025-05-02 19:37:43 +0800

    update the section

--- app/pricing/(comp)/(dry-clean)/z-dry-clean-list.js
+++ app/pricing/(comp)/(dry-clean)/z-dry-clean-list.js
@@ -1,121 +1,527 @@
 export const dryCleanList = [
   {
-    name: "Wash, Dry and Fold",
+    name: "Shirt / T-Shirt / Polo-T",
     imageSrc:
-      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/0b28bc1e-58af-407f-24d5-fea5be434200/S",
-    price: `RM 4.50 / kg`,
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/c7ff9f99-1f5a-487f-0c8c-5b584b406f00/S",
+    price: `RM 8.00 / piece`,
     others: "",
   },
   {
-    name: "Dry and Fold only",
+    name: "Blouse",
     imageSrc:
-      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/d37a7ce9-367a-46ea-ed02-39adf8097400/S",
-    price: `RM 3.50 / kg`,
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/7090dc65-98b2-417e-22dc-717fc5271000/S",
+    price: `RM 8.00 / piece`,
     others: "",
   },
   {
-    name: "Hand Wash",
+    name: "Jumpsuit",
     imageSrc:
-      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/913a43bd-f750-41b9-7c1f-837f1d459000/S",
-    price: `RM 3.00 / pc`,
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/e74ab43f-6c6a-4806-5697-5fd1ec79d200/S",
+    price: `RM 15.00 / piece`,
     others: "",
   },
   {
-    name: "Regular Toy",
+    name: "Short Pants / Short Jeans",
     imageSrc:
-      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/66310b2e-4230-45c3-f543-2e59ec77a700/S",
-    price: `RM 15.00 / pc`,
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/1a378171-4f3a-45ab-8375-5322cc535a00/S",
+    price: `RM 8.00 / piece`,
     others: "",
   },
   {
-    name: "Big Toy",
+    name: "Long Pants / Long Jeans",
     imageSrc:
-      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/1453b017-8c5b-43fb-8bf6-28bfe2f8f800/S",
-    price: `RM 25.00 / pc`,
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/5c199366-dd37-4e05-cc9b-a449d477a100/S",
+    price: `RM 8.00 / piece`,
     others: "",
   },
   {
-    name: "Bedsheet",
+    name: "Jogger Pants / Sport Pants",
     imageSrc:
-      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/bf05c583-9f38-4bc3-e9a1-febad709af00/S",
-    price: `RM 7.00 / set`,
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/b908cd07-1de7-4563-3ba5-322900f6cd00/S",
+    price: `RM 8.00 / piece`,
     others: "",
   },
   {
-    name: "Regular Pillow",
+    name: "Short Skirts / Short Jean Skirts",
     imageSrc:
-      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/2fccd3ca-592d-4726-a836-9c844793a500/S",
-    price: `RM 15.00 / set`,
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/dad34d33-22d6-4640-9bd0-b639c3b5ab00/S",
+    price: `RM 8.00 / piece`,
     others: "",
   },
   {
-    name: "Big Pillow",
+    name: "Mesh / Puffy / Tulle Skirt",
     imageSrc:
-      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/ddc6e58b-887b-4986-2765-808204130200/S",
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/edb5a1c7-5312-4fff-6d82-c263cb2f1b00/S",
+    price: `RM 10.00 / piece`,
+    others: "",
+  },
+  {
+    name: "Long Skirts / Long Jean Skirts / Long Tulle Skirt",
+    imageSrc:
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/a581daba-9f9a-436f-1fc8-948699367900/S",
+    price: `RM 15.00 / piece`,
+    others: "",
+  },
+  {
+    name: "Hat / Wool Hat",
+    imageSrc:
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/72b72c9b-2012-4cb1-daa1-262ff2452500/S",
+    price: `RM 8.00 / piece`,
+    others: "",
+  },
+  {
+    name: "Winter Hat / Beanie",
+    imageSrc:
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/c1e97aa4-969b-4306-173b-701bd8e73d00/S",
+    price: `RM 8.00 / piece`,
+    others: "",
+  },
+  {
+    name: "Winter Hat with Ear Flap",
+    imageSrc:
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/10322204-bc4b-4cf9-0244-742ffe5c1200/S",
+    price: `RM 10.00 / piece`,
+    others: "",
+  },
+  {
+    name: "Winter Hat with Extended Ear Flap",
+    imageSrc:
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/54b2e514-e067-4510-b609-7c414a286000/S",
+    price: `RM 10.00 / piece`,
+    others: "",
+  },
+  {
+    name: "Suit Jacket / Suit Coat / Blazer",
+    imageSrc:
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/3178faeb-c5dc-450c-8376-b26db08f5700/S",
+    price: `RM 18.00 / piece`,
+    others: "",
+  },
+  {
+    name: "Vest / Waistcoat",
+    imageSrc:
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/e94ac197-9b30-465c-767c-b391e1127600/S",
+    price: `RM 12.00 / piece`,
+    others: "",
+  },
+  {
+    name: "Suit Pants / Trousers",
+    imageSrc:
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/cb47b7fd-6261-4e2d-5ecd-0fc2c0997100/S",
+    price: `RM 8.00 / piece`,
+    others: "",
+  },
+  {
+    name: "Tie",
+    imageSrc:
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/606f1e04-697b-41d4-1aad-a9d1ef597700/S",
+    price: `RM 5.00 / piece`,
+    others: "",
+  },
+  {
+    name: "Scarf / Silk Scarf",
+    imageSrc:
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/faceafdb-691e-488d-85b5-2678c8b4b400/S",
+    price: `RM 8.00 / piece`,
+    others: "",
+  },
+  {
+    name: "Regular Jacket",
+    imageSrc:
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/1b2661a9-66a3-4256-07d0-29c2fc7da700/S",
+    price: `RM 15.00 / piece`,
+    others: "With Hood + RM 2",
+  },
+  {
+    name: "Hoodie",
+    imageSrc:
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/fa96affd-1992-4b32-d03a-68d7d6ae8c00/S",
+    price: `RM 15.00 / piece`,
+    others: "",
+  },
+  {
+    name: "Cardigan",
+    imageSrc:
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/88e8c59d-692e-465f-e589-2ab050a86800/S",
+    price: `RM 18.00 / piece`,
+    others: "",
+  },
+  {
+    name: "Jeans Jacket",
+    imageSrc:
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/29dc2e0a-2cc5-4a8a-adea-cfe6a1499f00/S",
+    price: `RM 15.00 / piece`,
+    others: "",
+  },
+  {
+    name: "Soft Shell Jacket",
+    imageSrc:
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/33fd12fe-47cd-49d0-2f63-b2d7d8e51300/S",
+    price: `RM 15.00 / piece`,
+    others: "",
+  },
+  {
+    name: "Fleece Jacket",
+    imageSrc:
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/be285d5f-5d7a-4ba6-d0d1-b552c0ed9300/S",
+    price: `RM 18.00 / piece`,
+    others: "",
+  },
+  {
+    name: "Leather Jacket",
+    imageSrc:
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/bce8c3c2-b3a2-443e-9c26-9d71a126b800/S",
+    price: `RM 18.00 / piece`,
+    others: "",
+  },
+  {
+    name: "Fur-Lined Jacket",
+    imageSrc:
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/3ce259d1-96a8-4b65-60e5-3cbafc5e0a00/S",
+    price: `RM 25.00 / piece`,
+    others: "With Hood + RM 2",
+  },
+  {
+    name: "Fur-Lined Jacket + Fluffy Faux Fur Collar",
+    imageSrc:
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/ec1ab66d-3eed-4358-e35a-094d4dc04500/S",
+    price: `RM 30.00 / piece`,
+    others: "",
+  },
+  {
+    name: "Fur-Lined Jacket + Extended Fur Collar",
+    imageSrc:
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/2abd822f-5e91-47d6-51b1-5239249fe000/S",
+    price: `RM 35.00 / piece`,
+    others: "",
+  },
+  {
+    name: "Light Down Jacket",
+    imageSrc:
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/4ebeb556-a605-48d1-f946-122bcba50f00/S",
+    price: `RM 18.00 / piece`,
+    others: "Extended Length (over knee) + RM 5",
+  },
+  {
+    name: "Light Down Jacket / Vest (NO SLEEVE)",
+    imageSrc:
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/7949d4c3-926f-4b4c-1507-fac1337c8500/S",
+    price: `RM 15.00 / piece`,
+    others: "Extended Length (over knee) + RM 5",
+  },
+  {
+    name: "Light Down Jacket with Hood ",
+    imageSrc:
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/690f2fa5-0381-463d-9c05-9fac681f1800/S",
+    price: `RM 23.00 / piece`,
+    others: "Extended Length (over knee) + RM 5",
+  },
+  {
+    name: "Winter Scarf",
+    imageSrc:
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/7c390165-fa76-4d4b-1c41-bf67824ec300/S",
+    price: `RM 8.00 / piece`,
+    others: "",
+  },
+  {
+    name: "Winter Glove Fabric / Leather / Polyester",
+    imageSrc:
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/11f0cd74-934e-4229-c4ce-00e325a26f00/S",
+    price: `RM 8.00 / piece`,
+    others: "",
+  },
+  {
+    name: "Long Winter Glove Fabric / Leather",
+    imageSrc:
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/eea87f5c-6f56-4b97-34e3-dae308450e00/S",
+    price: `RM 10.00 / piece`,
+    others: "",
+  },
+  {
+    name: "Snow Pants",
+    imageSrc:
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/5969c297-ffc3-4b46-8937-4c24f6ad7100/S",
+    price: `RM 10.00 / piece`,
+    others: "",
+  },
+  {
+    name: "Sweater / Knitwear",
+    imageSrc:
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/0a680eb6-c9c0-40b9-0e9f-29f601b48a00/S",
+    price: `RM 15.00 / piece`,
+    others: "",
+  },
+  {
+    name: "Wool Sweater",
+    imageSrc:
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/b8114b3c-2b67-4078-b0df-c3f119c28b00/S",
+    price: `RM 15.00 / piece`,
+    others: "",
+  },
+  {
+    name: "Chunky Wool Sweater (Raw Organic Wool)",
+    imageSrc:
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/e82eaa08-32e9-49de-5270-1decc0a1e200/S",
+    price: `RM 22.00 / piece`,
+    others: "",
+  },
+  {
+    name: "Fluffy Faux Fur Sweater",
+    imageSrc:
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/d1db6e2e-d708-41ae-f2c4-0ee24ef7b100/S",
+    price: `RM 22.00 / piece`,
+    others: "",
+  },
+  {
+    name: "Classic Winter Jacket",
+    imageSrc:
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/4b0a2f56-5359-4576-8e8a-8ad9fa2d5400/S",
+    price: `RM 18.00 / piece`,
+    others: "With Hood + RM 2",
+  },
+  {
+    name: "Wind-blocking outer layer jacket",
+    imageSrc:
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/ad628580-f094-4935-c445-b35fa09b0e00/S",
+    price: `RM 22.00 / piece`,
+    others: "With Hood + RM 2",
+  },
+  {
+    name: "Goose Down Jacket (Thick)",
+    imageSrc:
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/51a855f8-12c0-4fc6-9be2-0657165d1700/S",
+    price: `RM 30.00 / piece`,
+    others: "With Fur-lined Hood + RM 10",
+  },
+  {
+    name: "Full Length Goose Down Jacket (Thick)",
+    imageSrc:
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/495a0b1c-9661-4493-ed17-c056cefe9100/S",
+    price: `RM 35.00 / piece`,
+    others: "With Fur-lined Hood + RM 10",
+  },
+  {
+    name: "Goose Down Fur-lined Jacket (Thick)",
+    imageSrc:
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/5888faf2-3ffd-4ea3-be1f-3b369455e800/S",
+    price: `RM 35.00 / piece`,
+    others: "With Fur-lined Hood + RM 10",
+  },
+  {
+    name: "Short Coat / Short Wool Coat",
+    imageSrc:
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/ba7ae693-7ae2-4b98-cb13-69007a7fc400/S",
+    price: `RM 25.00 / piece`,
+    others: "",
+  },
+  {
+    name: "Long Coat / Long Wool Coat/ Overcoat",
+    imageSrc:
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/e202dae3-1a98-4136-3fb7-073c229a1500/S",
+    price: `RM 30.00 / piece`,
+    others: "",
+  },
+  {
+    name: "Extended Long Coat / Long Overcoat",
+    imageSrc:
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/244d7670-2bb6-47fb-7fa3-94ec05774900/S",
+    price: `RM 40.00 / piece`,
+    others: "",
+  },
+  {
+    name: "Trench Coat Short",
+    imageSrc:
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/677a6107-1323-48f6-b6a3-9363e9420700/S",
+    price: `RM 25.00 / piece`,
+    others: "",
+  },
+  {
+    name: "Trench Coat",
+    imageSrc:
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/501bc5a8-88dc-4838-ba16-58f30bbdf200/S",
+    price: `RM 35.00 / piece`,
+    others: "",
+  },
+  {
+    name: "Faux Mink Fur Coat",
+    imageSrc:
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/d8d1a9ce-185e-4ccd-32d6-1eadbf6c8400/S",
+    price: `RM 50.00 / piece`,
+    others: "",
+  },
+  {
+    name: "Faux Chinchilla Fur Coat",
+    imageSrc:
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/5261093f-2d1a-4296-8f2b-e348c3a65a00/S",
+    price: `RM 50.00 / piece`,
+    others: "",
+  },
+  {
+    name: "Saree",
+    imageSrc:
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/efc1447d-38c1-4859-ac4d-92007066b900/S",
+    price: `RM 12.00 / piece`,
+    others: "",
+  },
+  {
+    name: "Saree Top / Saree Blouses",
+    imageSrc:
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/82eb64f2-88df-43b0-61f6-eb8608d1cd00/S",
+    price: `RM 8.00 / piece`,
+    others: "",
+  },
+  {
+    name: "Saree Scarf",
+    imageSrc:
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/e9cb9ea8-94a5-4421-2dc5-4d5d1fe90b00/S",
+    price: `RM 10.00 / piece`,
+    others: "",
+  },
+  {
+    name: "Saree Manik",
+    imageSrc:
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/ad18a048-edbe-4ec8-bf10-84d6c78e1900/S",
+    price: `RM 14.00 / piece`,
+    others: "",
+  },
+  {
+    name: "Punjabi Suit (Women)",
+    imageSrc:
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/db7a5320-995d-478c-4097-6a6b2477e700/S",
     price: `RM 25.00 / set`,
+    others: "2 pieces + RM 0 , 3 pieces + RM 3",
+  },
+  {
+    name: "Lehenga",
+    imageSrc:
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/17e9a5f3-48d0-4f81-4fe2-fb41cca1ad00/S",
+    price: `RM 25.00 / set`,
+    others: "2 pieces + RM 0 , 3 pieces + RM 3",
+  },
+  {
+    name: "Punjabi Suit (Men)",
+    imageSrc:
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/eb1f95a9-d95e-411e-b520-2d4a3bcc1000/S",
+    price: `RM 23.00 / set`,
+    others: "2 pieces + RM 0 , 3 pieces + RM 3",
+  },
+  {
+    name: "Veshti / vēṭṭi",
+    imageSrc:
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/a4fe140d-adf5-425d-54be-f4b7caad3b00/S",
+    price: `RM 8.00 / piece`,
     others: "",
   },
   {
-    name: "Comforter - S",
+    name: "Veshti / vēṭṭi Long",
     imageSrc:
-      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/86f7af4f-9076-4140-39cd-96d5318dc900/S",
-    price: `RM 13.00 / pc`,
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/5011ec04-8a2f-4990-7e2c-cf677d9ddc00/S",
+    price: `RM 13.00 / piece`,
     others: "",
   },
   {
-    name: "Comforter - Q,K",
+    name: "Baju Kurung",
     imageSrc:
-      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/9cfad004-1ee0-4f0f-f4de-e8338e903500/S",
-    price: `RM 14.00 / pc`,
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/5c39fdac-12d3-439e-365f-84bd7f09bc00/S",
+    price: `RM 20.00 / set`,
+    others: "2 pieces + RM 0 , 3 pieces + RM 3",
+  },
+  {
+    name: "Baju Kurung - Top Only",
+    imageSrc:
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/22a48422-3e32-4ed9-6dd3-d60858f49000/S",
+    price: `RM 14.00 / piece`,
     others: "",
   },
   {
-    name: "Duvet (Comforter Cover) - S,Q,K",
+    name: "Baju Kurung - Bottom Only",
     imageSrc:
-      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/2a33924c-5b09-4528-c85f-102eebdc6900/S",
-    price: `RM 12.00 / pc`,
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/161ac17a-d2a6-427e-be5e-99f0068cda00/S",
+    price: `RM 8.00 / piece`,
     others: "",
   },
   {
-    name: "Mattress Protector - S,Q,K",
+    name: "Baju Kebaya",
     imageSrc:
-      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/91af6edf-4db8-4724-5e14-04ddf35b5500/S",
-    price: `RM 14.00 / pc`,
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/c5227a3e-50df-4d26-1a01-99feaf914500/S",
+    price: `RM 22.00 / piece`,
+    others: "2 pieces + RM 0 , 3 pieces + RM 3",
+  },
+  {
+    name: "Baju Kebaya - Top only",
+    imageSrc:
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/7ac74aaa-e2fc-477c-1b36-08b7f9004c00/S",
+    price: `RM 16.00 / piece`,
     others: "",
   },
   {
-    name: "Floor Matt",
+    name: "Baju Kebaya - bottom only",
     imageSrc:
-      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/57a95206-7eac-4029-8730-6ab055565e00/S",
-    price: `RM 8.00 / kg`,
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/aec88662-90e2-4dfc-a8a9-90be87e28300/S",
+    price: `RM 8.00 / piece`,
     others: "",
   },
   {
-    name: "Table Cloth",
+    name: "Baju Melayu",
     imageSrc:
-      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/738f0505-3ea7-4aa2-c7f9-efc807da2900/S",
-    price: `RM 8.00 / kg`,
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/475d2526-70ec-4d0c-8483-b6e3d74afc00/S",
+    price: `RM 20.00 / set`,
+    others: "2 pieces + RM 0 , 3 pieces + RM 3",
+  },
+  {
+    name: "Jubah - Male",
+    imageSrc:
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/45fc1eda-844e-479a-f810-64561f051100/S",
+    price: `RM 18.00 / set`,
+    others: "2 pieces + RM 0 , 3 pieces + RM 3",
+  },
+  {
+    name: "Jubah / Abaya - Female",
+    imageSrc:
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/44e44b5f-d3ee-40c4-5054-57f55133e100/S",
+    price: `RM 20.00 / set`,
+    others: "2 pieces + RM 0 , 3 pieces + RM 3",
+  },
+  {
+    name: "Telekung",
+    imageSrc:
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/6904308d-6941-49eb-b279-03ba997ca000/S",
+    price: `RM 20.00 / set`,
+    others: "2 pieces + RM 0 , 3 pieces + RM 3",
+  },
+  {
+    name: "Cheongsam / 旗袍 / 長袍",
+    imageSrc:
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/a443d5c2-7663-42e8-0a8a-4d7731712900/S",
+    price: `RM 25.00 / piece`,
     others: "",
   },
   {
-    name: "Cushion Cover",
+    name: "Cheongsam - Top Only / Cheongsam Coat",
     imageSrc:
-      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/7711c75c-534d-4ee6-ed6a-58dc39f23f00/S",
-    price: `RM 8.00 / kg`,
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/700446a6-7907-455d-d2c6-d8f483aa8700/S",
+    price: `RM 18.00 / piece`,
     others: "",
   },
   {
-    name: "Military / Army Uniform",
+    name: "Changsan / 長衫",
     imageSrc:
-      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/721138e5-ae72-4991-0955-be92cd117100/S",
-    price: `RM 27.00 / set`,
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/56955a64-16f6-4941-de50-ed6a5d5b3000/S",
+    price: `RM 18.00 / piece`,
     others: "",
   },
   {
-    name: "Tentera Darat (~Pakaian No. 3) Uniform",
+    name: "Evening Gown / Evening Dress",
     imageSrc:
-      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/24c1e784-4774-443e-237c-d1801b58ca00/S",
-    price: `RM 27.00 / set`,
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/5ec9b9e2-4469-42ff-f890-820ccdc8c300/S",
+    price: `From RM 65 / piece`,
+    others: "",
+  },
+  {
+    name: "Wedding Gown",
+    imageSrc:
+      "https://imagedelivery.net/_rcIVT3taNSHudAfnOfnXg/3534c953-6bcb-4259-e917-4c4af58a5f00/S",
+    price: `From RM 65 / piece`,
     others: "",
   },
 ];
